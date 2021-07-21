83ac526aeed3ecba830d9574b65f8d82621ce609
Pauline GB <pauline.gb@protonmail.com>
2021-07-21 18:32:09 +0200

    configure admin dashboard

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -2,11 +2,18 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Image;
+use App\Entity\Menace;
+use App\Entity\Story;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Assets;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class AdminController extends AbstractDashboardController
 {
@@ -15,18 +22,26 @@ class AdminController extends AbstractDashboardController
      */
     public function index(): Response
     {
-        return parent::index();
+        $routeBuilder = $this->get(CrudUrlGenerator::class)->build();
+
+        return $this->redirect($routeBuilder->setController(StoryCrudController::class)->generateUrl());
     }
 
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('SPC');
+            ->setTitle('| SPC |');
     }
 
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linktoRoute('Retour sur le site', 'fa fa-home', 'home');
+        yield MenuItem::linkToRoute('Déconnexion', 'fa fa-sign-out', 'logout');
+        yield MenuItem::section('Menu');
+        yield MenuItem::linkToCrud('Rapports SCP', '', Story::class);
+        yield MenuItem::linkToCrud('Menaces', '', Menace::class);
+        yield MenuItem::linkToCrud('Images', '', Image::class);
+        yield MenuItem::linkToCrud('Utilisateurs', '', User::class);
     }
 }
